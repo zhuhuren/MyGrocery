253fa022cba76a3ace395aa9e4269d178f627552
MyFridge Builder <builder@myfridge.local>
2026-09-07 11:17:46 -0600

    cleanup

--- empty_segments.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let text = fs.readFileSync('index.html', 'utf8');
-
-text = text.replace(/<div class="segmented-control" id="inventory-locations">[\s\S]*?<\/div>/, '<div class="segmented-control" id="inventory-locations"><\/div>');
-text = text.replace(/<div class="segmented-control" id="receipt-location"[^>]*>[\s\S]*?<\/div>/, '<div class="segmented-control" id="receipt-location" style="margin-top: 4px;"><\/div>');
-text = text.replace(/<div class="segmented-control" id="form-location">[\s\S]*?<\/div>/, '<div class="segmented-control" id="form-location"><\/div>');
-
-fs.writeFileSync('index.html', text, 'utf8');
--- update_cats.js
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs');
-let code = fs.readFileSync('app.js', 'utf8');
-
-code = code.replace(/CATEGORIES\.find/g, 'getAllCategories().find');
-code = code.replace(/CATEGORIES\.forEach/g, 'getAllCategories().forEach');
-code = code.replace(/CATEGORIES\.map/g, 'getAllCategories().map');
-code = code.replace(/CATEGORIES\[CATEGORIES\.length - 1\]/g, 'getAllCategories()[getAllCategories().length - 1]');
-
-fs.writeFileSync('app.js', code, 'utf8');
